fb9d6b0d64180e2c00fbe5ed1f2946a9d3b24975
Bo-Cheng Huang <43872689+BanarryHuang@users.noreply.github.com>
2019-06-24 13:40:26 +0800

    Update 72-3.cpp

--- 72-3.cpp
+++ 72-3.cpp
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
-
+using namespace std;
 int main(){
     int i,j,x,f;
     char b[300],d[30];
     fgets(b,300,stdin);
-    scanf("%s%d",d,&x);
+    cin >> d >> x;
     for(j=0;j<x;j++){
         f=0;
         for(i=strlen(b)-2;i>-1;){
             if(f==1&&i%x==j){
-                printf("%c%c",d[0],b[i]);
+                cout << d[0] << b[i];
                 i-=x;
             }
             if(f==0&&i%x==j){
-                printf("%c",b[i]);
+                cout << ' ' << b[i];
                 i-=x;
                 f=1;
             }
-            if(f==0)i--;
+            if(f==0)
+                i--;
         }
-        printf("\n");
+        cout << endl;
     }
 
     return 0;
