ada3a74288d408c34fd61c3c9808215772daaf4c
Bo-Cheng Huang <43872689+BanarryHuang@users.noreply.github.com>
2019-06-24 10:40:30 +0800

    Update Knapsack.cpp

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -1,24 +1,29 @@
 #include <bits/stdc++.h>
 #define n 1002
-
+using namespace std;
 typedef struct dish{
     int happy;
     int weight;
 }D;
 int main(){
 
-    int i,j,k,N,W,max=0;
+    int i,j,k,N,W,maxw=0;
     D d[501];
     int w[501][n];
 
-    scanf("%d%d",&N,&W);
+    cin >> N;
 
-    for(i=0;i<N;i++)
-        for(j=1;j<=W;j++)
+    for(i=0;i<n;i++)
+        for(j=1;j<=n;j++)
             w[i][j]=0;
 
     for(i=0;i<N;i++){
-        scanf("%d%d",&(d[i].happy),&(d[i].weight));
+        cin >> d[i].weight >> d[i].happy;
+    }
+
+    cin >> W;
+
+    for(i=0;i<N;i++){
         if(i==0)
             w[0][d[i].weight]=d[0].happy;
         if(i>0){
@@ -31,10 +36,10 @@ int main(){
     }
 
     for(j=1;j<=W;j++)
-        if(max<w[N-1][j])
-            max=w[N-1][j];
+        if(maxw<w[N-1][j])
+            maxw=w[N-1][j];
 
-    printf("%d\n",max);
+    cout << maxw;
 
     return 0;
 }
