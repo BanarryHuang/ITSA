a755a8da590e35a4285e8d23f7e4a8087adb2fdb
Bo-Cheng Huang <43872689+BanarryHuang@users.noreply.github.com>
2019-06-26 02:18:19 +0800

    Update 71-2.cpp

--- 71-2.cpp
+++ 71-2.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
-
+using namespace std;
 int main()
 {
-    int i,j,T,n,k,l,min,all;
+    int i,j,T,n,k,l,mini,all;
     int a[101][101];
     int b[101];
-    scanf("%d",&T);
+    cin >> T;
     while(T--){
-        scanf("%d %d",&n,&k);
+        cin >> n >> k;
         for(i=0;i<n;i++)
             for(j=0;j<n;j++)
-                scanf("%d",&(a[i][j]));
-
-        printf("%d",k);
+                cin >> a[i][j];
+        cout << k;
         all=1;
         while(all<n){
-            min=10000;
+            mini=10000;
             if(all==1){
                 for(i=0;i<n;i++){
                     b[i]=a[k][i];
@@ -30,21 +29,19 @@ int main()
                     continue;
                 if(b[i]>a[k][i])
                     b[i]=a[k][i];
-                if(b[i]<min){
-                    min=b[i];
+                if(b[i]<mini){
+                    mini=b[i];
                     l=i;
                 }
             }
-            printf(" %d",l);
-            for(j=0;j<n;j++){
+            cout << " " << l;
+            for(j=0;j<n;j++)
                 a[j][l]=10000;
-            }
             b[l]=10000;
             k=l;
             all++;
         }
-    printf("\n");
+        cout << endl;
     }
-
     return 0;
 }
