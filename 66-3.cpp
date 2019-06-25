0135deb99c622231e45eecccecf43113f4c7f711
Bo-Cheng Huang <43872689+BanarryHuang@users.noreply.github.com>
2019-06-26 02:10:06 +0800

    Update 66-3.cpp

--- 66-3.cpp
+++ 66-3.cpp
@@ -2,23 +2,24 @@
 using namespace std;
 int main()
 {
-    int T,i,copy;
+    int T,i,copy,count;
     char deli[]=";:,";
-    scanf("%d",&T);
+    cin >> T;
     while(T--){
-    char path[40];
-    char file[40][40];int count=0;
-    char *start=path;
-    cin >> path;
-    start=strtok(start,deli);
-    while(start!=NULL){
-        strcpy(file[cont],start);
-        count++;
-        start=strtok(NULL,deli);
-    }
-    printf("Tokens found:\n");
-    for(i=0;i<count;i++)
-        cout << file[i] << endl;
+        count=0;
+        char path[40];
+        char file[40][40];
+        char *start=path;
+        cin >> path;
+        start=strtok(start,deli);
+        while(start!=NULL){
+            strcpy(file[count],start);
+            count++;
+            start=strtok(NULL,deli);
+        }
+        printf("Tokens found:\n");
+        for(i=0;i<count;i++)
+            cout << file[i] << endl;
     }
     return 0;
 }
