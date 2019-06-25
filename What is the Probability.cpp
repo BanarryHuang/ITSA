85d38a4671e09a002b95c94cc5c4c3c71d8061cb
Bo-Cheng Huang <43872689+BanarryHuang@users.noreply.github.com>
2019-06-26 03:07:44 +0800

    Update What is the Probability.cpp

--- What is the Probability.cpp
+++ What is the Probability.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
-
+using namespace std;
 int main(){
 
     int S,N,w,i;
     float p,all,x,y;
 
-    scanf("%d",&S);
+    cin >> S;
 
     while(S--){
-        scanf("%d%f%d",&N,&p,&w);
+        cin >> N >> p >> w;
         all=0;x=1;
         for(i=0;i<N;i++){
-            if((w-1)==i)y=x;
+            if(w-1==i)
+                y=x;
             all+=x;
             x*=(1.0-p);
         }
-
-        printf("%.4f\n",y/all);
+        cout << setprecision(4) << y/all << endl;
     }
-
-
 }
