3f296a5dbbeb808ff13c68d0789430613b9fd903
Bo-Cheng Huang <43872689+BanarryHuang@users.noreply.github.com>
2019-06-26 10:35:28 +0800

    Update The_Number.cpp

--- The_Number.cpp
+++ The_Number.cpp
@@ -6,10 +6,11 @@ int main()
     char x1,x2;
     int i,count;
 
-    while(scanf("%c%c ",&x1,&x2)!=EOF){
+    while(!cin.eof()){
+        cin >> x1 >> x2;
     	char str[8]={0};
     	count=0;
-    	gets(str);
+    	cin >> str;
     	for(i=0;i<7;i++){
         	if(str[i]==x1&&str[i+1]==x2)
         		count++;
